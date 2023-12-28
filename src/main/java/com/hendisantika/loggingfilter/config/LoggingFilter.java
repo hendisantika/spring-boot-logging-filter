68e408688dcc7f732ed62a258c6b3dc4cc6a6d4a
hendisantika <hendisantika@yahoo.co.id>
2023-12-28 07:52:58 +0700

    Add LoggingFilter🫸🌀✏️ 📗 :octocat:

---
+++ src/main/java/com/hendisantika/loggingfilter/config/LoggingFilter.java
@@ -0,0 +1,21 @@
+package com.hendisantika.loggingfilter.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-logging-filter
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/28/23
+ * Time: 07:52
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+
+}
